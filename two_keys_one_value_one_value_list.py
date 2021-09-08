ab4f942910daed8cce854d6ccd77bef433193c0e
An <90125508+AldosHub@users.noreply.github.com>
2021-09-07 22:25:06 -0700

    Add files via upload

---
+++ two_keys_one_value_one_value_list.py
@@ -0,0 +1,12 @@
+cities = {
+    'City': 'Sanfrancisco', 'Facts': {
+        'Population': 'Millions', 'Density':'High',
+        },
+    'City': 'Tulare', 'Facts': {
+        'Population': 'Thousands', 'Density':'Low',
+        },
+    'City': 'New York', 'Facts': {
+        'Population': 'Billions', 'Density':'Very High',}
+}
+for key, value in cities.items():
+    print(value)
